46ac942c9265ac6e8bdf78ab235a80f1cff5fa60
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-08-28 13:56:41 +0200

    Forelesing 1

--- module-1/example_01.mjs
+++ module-1/example_01.mjs
@@ -1,6 +1,10 @@
 "use strict"
 import { printOut } from "../../common/script/utils.mjs";
 
-const PI = 3.1415926547;
-const a = 12
- printOut({PI, a})
+let a = 36; // Timer
+let days = a / 24;
+const day = Math.floor(days);
+const rest = days - day;
+const hours = rest * 24;
+
+printOut({a, days, day, rest, hours});
