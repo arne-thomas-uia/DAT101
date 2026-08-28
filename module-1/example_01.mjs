28f4008c02184e7a8641e4dda5749fbc6ca866af
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-08-28 14:24:57 +0200

    Forelesning 3

--- module-1/example_01.mjs
+++ module-1/example_01.mjs
@@ -1,7 +1,10 @@
 "use strict"
 import { printOut } from "../../common/script/utils.mjs";
 
-let a = 5.01;
-let b = Math.ceil(a);
-
-printOut({a, b});
+let a = 5;
+const text = `Variable a inneholder ${a}`;
+const l = text.length;
+printOut( l );
+const text2 = text.substring(0, 10);
+printOut ( text );
+printOut ( text2 );
