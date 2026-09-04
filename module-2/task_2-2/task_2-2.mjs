22e0d25e5ab0ecd79b04b04f7d48a88d76888af0
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-04 15:42:58 +0200

    Example 2.2 part 2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -3,11 +3,31 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut(newLine);
+// Use JavaScript to calculate the following expression, ensuring the result is -34:
+// 
+const orgMatExp = "2 + 3 * 2 - 4 * 6";
+const newMathExp = "2 + (3 * 2 - 4) * 6";
+const part1Answer = 2 + (3 * 2 - 4) * 6;
+printOut(orgMatExp);
+printOut(newMathExp);
+printOut(part1Answer);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Convert 25 metres and 34 centimeters to inches.
+An inch is 25.4 millimeters (maximum 2 decimal places in the answer). */
+
+// This is with help variables.
+const oneInch = 25.4 // millimeters
+const metersInMillimeters = 25 * 1000;
+const centimetersInMillimeters = 34 * 10;
+const millimeters = metersInMillimeters + centimetersInMillimeters
+const inches = millimeters / oneInch;
+printOut(`25 meters = ${inches.toFixed(2)} inches`);
+
+// This is without help variables.
+const inches_2 = ((25*1000 + 34*10) / 25.4).toFixed(2); 
+printOut (`25 meters = ${inches_2} inches`);
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
