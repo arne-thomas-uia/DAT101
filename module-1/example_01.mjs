2af46896b5313c48b6ee8a1f7b0038375de61721
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-04 14:16:13 +0200

    Lecture about operands

--- module-1/example_01.mjs
+++ module-1/example_01.mjs
@@ -1,10 +1,11 @@
 "use strict"
 import { printOut } from "../../common/script/utils.mjs";
 
-let a = 5;
-const text = `Variable a inneholder ${a}`;
-const l = text.length;
-printOut( l );
-const text2 = text.substring(0, 10);
-printOut ( text );
-printOut ( text2 );
+const person = 
+{
+    name: "Arne",
+    age: 25,
+    height: 178 
+};
+
+printOut( person.height );
--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -1,4 +1,14 @@
-
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
 
+let a = 5;
+
+printOut( a );
+
+a = 4 + 4;
+
+printOut( a );
+
+a += 2; // Dette er det samme som a = a + 2;
+
+printOut( a );
