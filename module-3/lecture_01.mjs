6c3ff42dd03ff91ed6a46f0121ba25754cbbf488
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-11 12:54:29 +0200

    Eksempel på If test

--- module-3/lecture_01.mjs
+++ module-3/lecture_01.mjs
@@ -1,5 +1,21 @@
-"use strict"
-import {printOut} from "../../common/script/utils.mjs";
+"use strict";
+import { printOut } from "../../common/script/utils.mjs";
+const txtAge = document.getElementById("txtAge");
+let age;
+document.getElementById("btnSubmit").addEventListener("click", CheckAge);
+const movieAgeLim = 16;
 
+printOut(`Du må være ${movieAgeLim} år, for å se filmen.` )
 
+function CheckAge() {
+  age = parseInt(txtAge.value);
+  
+  if(age === movieAgeLim){
+    printOut(`Du er akkurat ${movieAgeLim} år.`);
+  }else if(age > movieAgeLim){
+    printOut(`Du er eldre enn ${movieAgeLim} år.`);
+  }else{
+    printOut("Du er ikke eldre enn " + movieAgeLim + " år.");
+  }
 
+}
