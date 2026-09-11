5b1dc09dd62f1a5c7fb2b56a80422220cf72aa6c
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-11 15:33:39 +0200

    Eksempel Random

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -1,14 +1,9 @@
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
+// 20 >= r <= 25
+const r = Math.floor(Math.random() * 5) + 20;
+printOut({ r });
 
-let a = 5;
-
-printOut( a );
-
-a = 4 + 4;
-
-printOut( a );
-
-a += 2; // Dette er det samme som a = a + 2;
-
-printOut( a );
+setTimeout(() => {
+    window.location.reload();
+}, 500);
