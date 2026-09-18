96b8479efd4337e982746abea908e40189015d7e
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-18 12:40:18 +0200

    For loop example 2

--- module-3/lecture_03.mjs
+++ module-3/lecture_03.mjs
@@ -1,9 +1,13 @@
 "use strict";
 import { printOut } from "../../common/script/utils.mjs";
 
-for(let i = 1; i <=20; i++){
- printOut(i);
+const tableDraw = [10, 15, 11, 9, 7 , 8, 31];
+
+let txtLottoDraw = "Ukens lotto trekning:";
+for(let i = 0; i < 7; i++){
+ const drawNumber = tableDraw[i];
+ txtLottoDraw += " " + drawNumber.toString();
 }
 
-printOut("Ferdig!");
+printOut(txtLottoDraw);
 
