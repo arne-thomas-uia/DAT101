a40846d3592b7989dc52f98cfd4423edfcacf0e8
Arne-Thomas Aas Søndeled <arne.t.sondeled@uia.no>
2026-09-18 15:49:00 +0200

    Example Task 3.1 part 10

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -34,40 +34,67 @@ printOut(`Image User Size = ${imageUserSize}`);
 if (imageUserSize >= imageMinSize) {
   if (imageUserSize <= imageMaxSize) {
     printOut("Thank you");
-  }else {
+  } else {
     printOut("Image is to large");
   }
 } else {
   printOut("Image is to small!");
 }
 
-if(imageUserSize > imageMaxSize){
-   printOut("Image is to large"); 
-}else if (imageUserSize < imageMinSize){
-   printOut("Image is to small!"); 
-}else{
-    printOut("Thank you");
+if (imageUserSize > imageMaxSize) {
+  printOut("Image is to large");
+} else if (imageUserSize < imageMinSize) {
+  printOut("Image is to small!");
+} else {
+  printOut("Thank you");
 }
-
-
 
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-const monthList =["January", "February", "Mars", "April", "Mai", 
-"Jun", "Juli", "August", "September", "October", "November", "December"];
+const monthList = ["January", "February", "Mars", "April", "Mai", "Jun", "Juli", "August", "September", "October", "November", "December"];
 const noOfMonth = monthList.length;
 const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
-
+printOut({monthName});
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+} else {
+  printOut("You don't need to take vitamin D");
+}
 
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+switch(monthName){
+  case "January":
+  case "Mars":
+  case "Mai":
+  case "Juli":
+  case "August":
+  case "October":
+  case "December":
+    printOut("31 Days in month.");
+    break;
+  case "February":
+    printOut("28 Days in month.");
+    break;
+  default:
+    printOut("30 Days in month.");
+}
 
 printOut(newLine);
 
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+
+if(monthName === "March" || (monthName === "May")){
+  printOut("Sorry, the gallery is closed!");
+}else if( monthName === "April"){
+  printOut("Sorry, Main gallery is closed, your are welcome into the premiss next dor!");
+}else{
+  printOut("Welcome to mu gallery! ❤️");
+}
+
+
+
 
 printOut(newLine);
